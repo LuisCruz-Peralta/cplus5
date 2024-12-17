cb42498c531e2393f689b0fe25c6bb1f76a69b61
LuisCruz-Peralta <luisc.cruzperalta@gmail.com>
2024-12-16 19:43:22 -0700

    Add files via upload

---
+++ pr5.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+  int x;
+  cout << "Please enter an integer value: "<< endl;
+  cin >> x;
+  if (x==100) //Condicional if
+  {
+  cout << "x is 100"<< endl;
+  }
+  else
+  {
+  cout << "x is not 100"<< endl;
+  }
+}
